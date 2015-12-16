9079a0000147d052882b4baf6e6eb983d2c62d6c
Traitortots <jker3169@740s-pl-522.local>
2015-12-16 08:48:54 -0700

    Added Project

---
+++ src/bot/controller/JaredBotRunner.java
@@ -0,0 +1,13 @@
+package bot.controller;
+
+public class JaredBotRunner 
+{
+	/**
+	 * @param args Unused in the robot
+	 */
+	public static void main(String[] args)
+	{
+		BotController myController = new BotController();
+		myController.start();
+	}
+}
