e2026df52c15249999bf886154c9e5c325367fc3
Traitortots <jker3169@740s-pl-546.local>
2016-01-28 07:37:10 -0700

    added code

--- src/bot/model/EV3Bot.java
+++ src/bot/model/EV3Bot.java
@@ -1,15 +1,50 @@
 package bot.model;
 
+import lejos.robotics.navigation.MovePilot;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
 public class EV3Bot {
 	
+	private String botMessage;
+	private int xPosition;
+	private int yPosition;
+	private long waitTime;
+	private MovePilot botPilot;
 	
 	public EV3Bot()
 	{
+		this.botMessage = "Jared codes jaredBot";
+		this.xPosition = 50;
+		this.yPosition = 50;
+		this.waitTime = 4000;
 		
+		setupPilot();
+		displayMessage();
 	}
 	
+
+
+	private void setupPilot() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public void driveRoom()
 	{
+		displayMessage("driveRoom");
+	}
+
+	
+	private void displayMessage() 
+	{
+		LCD.drawString(botMessage, xPosition, yPosition);
+		//Delay
+	}
+	
+	private void displayMessage(String string) 
+	{
+		
 		
 	}
 	
